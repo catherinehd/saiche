375499df35b63094669e8d1dce34e71a2475c06d
catherinehd <ruiman_jiang@163.com>
2017-12-26 18:52:22 +0800

    。

--- car/src/app/home/home-routing.module.ts
+++ car/src/app/home/home-routing.module.ts
@@ -6,10 +6,10 @@ import { IndexComponent } from './index/index.component';
 const homeRoutes: Routes = [
   { path: 'home', redirectTo: 'home/number' },
   { path: 'home/number', component: IndexComponent },
-  { path: 'home/bigsmall', component: IndexComponent },
-  { path: 'home/singledouble', component: IndexComponent },
-  { path: 'home/onetwo', component: IndexComponent },
-  { path: 'home/dragontiger', component: IndexComponent },
+  { path: 'home/bigsmall/:id', component: IndexComponent },
+  { path: 'home/singledouble/:id', component: IndexComponent },
+  { path: 'home/onetwo/:id', component: IndexComponent },
+  { path: 'home/dragontiger/:id', component: IndexComponent },
 ];
 
 @NgModule({
